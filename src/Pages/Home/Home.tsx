00cd465ab33a7f722a86ad2aadabe7b2d0154b54
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-02 22:53:14 +0000

    add sidebar

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,13 @@
+import Sidebar from "../../Components/Sidebar";
+
 function Home() {
-	return <div>Home</div>;
+	return (
+		<div className="flex ">
+			<Sidebar />
+			{/* //home-container */}
+			<div className="flex-[6]">container</div>
+		</div>
+	);
 }
 
 export default Home;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			borderWidth: {
+				DEFAULT: "1px",
+				0: "0",
+				1: "0.5px",
+				2: "2px",
+				3: "3px",
+				4: "4px",
+				6: "6px",
+				8: "8px",
+			},
+		},
 	},
 	plugins: [],
 };
