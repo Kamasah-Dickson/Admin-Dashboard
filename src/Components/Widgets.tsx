74239e3c48e6cca4a7ad0cbd24cc25a0de24e2a5
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-03 00:58:53 +0000

    add widgets

---
+++ src/Components/Widgets.tsx
@@ -0,0 +1,24 @@
+function Widgets() {
+	return (
+		<div className="flex flex-1 items-center justify-between gap-5 p-5">
+			<div className="flex w-full items-center justify-between shadow-md">
+				<div>left</div>
+				<div>right</div>
+			</div>
+			<div className="flex w-full items-center justify-between shadow-md">
+				<div>left</div>
+				<div>right</div>
+			</div>
+			<div className="flex w-full items-center justify-between shadow-md">
+				<div>left</div>
+				<div>right</div>
+			</div>
+			<div className="flex w-full items-center justify-between shadow-md">
+				<div>left</div>
+				<div>right</div>
+			</div>
+		</div>
+	);
+}
+
+export default Widgets;
