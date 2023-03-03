c284eae6ce21854e61316332b4e5bf98a44b5004
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-03 01:01:13 +0000

    update pages

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,17 @@
+import Navbar from "../../Components/Navbar";
 import Sidebar from "../../Components/Sidebar";
+import Widgets from "../../Components/Widgets";
 
 function Home() {
 	return (
 		<div className="flex ">
 			<Sidebar />
-			{/* //home-container */}
-			<div className="flex-[6]">container</div>
+			<div className="flex-[6]">
+				<Navbar />
+				<div className="flex">
+					<Widgets />
+				</div>
+			</div>
 		</div>
 	);
 }
