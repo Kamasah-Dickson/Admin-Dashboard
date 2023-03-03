126d904e9cb1fb2b7d5fc5096cfe904c114e3da5
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-03 16:40:28 +0000

    update Pages

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,15 +1,25 @@
+import Chart from "../../Components/Chart";
+import Featured from "../../Components/Featured";
 import Navbar from "../../Components/Navbar";
 import Sidebar from "../../Components/Sidebar";
 import Widgets from "../../Components/Widgets";
 
 function Home() {
 	return (
-		<div className="flex ">
+		<div className="mx-auto flex max-w-[1440px] ">
 			<Sidebar />
 			<div className="flex-[6]">
 				<Navbar />
-				<div className="flex">
-					<Widgets />
+				<div className="mt-5 flex">
+					<Widgets type="user" />
+					<Widgets type="order" />
+					<Widgets type="earnings" />
+					<Widgets type="balance" />
+				</div>
+				{/* //chat container */}
+				<div className="mt-5 flex items-center justify-between gap-3 px-5">
+					<Featured />
+					<Chart />
 				</div>
 			</div>
 		</div>
