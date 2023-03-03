0c3bbb36c76eb111e5b75d19122907de7441e4d8
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-03 23:14:15 +0000

    update list pages

--- src/Pages/List/List.tsx
+++ src/Pages/List/List.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Datatable from "../../Components/Datatable";
+import Navbar from "../../Components/Navbar";
+import Sidebar from "../../Components/Sidebar";
 
 function List() {
-	return <div>List</div>;
+	return (
+		<div className="flex w-full">
+			<Sidebar />
+			<div className="flex-[6]">
+				<Navbar />
+				<Datatable />
+			</div>
+		</div>
+	);
 }
 
 export default List;
