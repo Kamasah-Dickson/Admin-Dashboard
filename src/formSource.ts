be67aa24c84ea7df556d7d3fd0b600a1fcd9ce88
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-04 06:08:57 +0000

    add form source

---
+++ src/formSource.ts
@@ -0,0 +1,77 @@
+export const userInputs = [
+	{
+		id: "input1",
+		label: "Username",
+		type: "text",
+		placeholder: "kamasah_dickson",
+	},
+	{
+		id: "input4",
+		label: "Full name",
+		type: "text",
+		placeholder: "Kamasah Dickson",
+	},
+	{
+		id: "input2",
+		label: "Email",
+		type: "email",
+		placeholder: "kamasahdickson19@gmail.com",
+	},
+	{
+		id: "input3",
+		label: "Password",
+		type: "password",
+		placeholder: "sds",
+	},
+	{
+		id: "input4",
+		label: "Telephone",
+		type: "tel",
+		placeholder: "+2335945710",
+	},
+	{
+		id: "input5",
+		label: "Country",
+		type: "text",
+		placeholder: "GHANA",
+	},
+	{
+		id: "input6",
+		label: "Address",
+		type: "text",
+		placeholder: "Elton St.216 NewYork",
+	},
+];
+
+export const productInputs = [
+	{
+		id: "input11",
+		label: "Title",
+		type: "text",
+		placeholder: "Apple Macbook Pro",
+	},
+	{
+		id: "input12",
+		label: "Description",
+		type: "text",
+		placeholder: "Description",
+	},
+	{
+		id: "input13",
+		label: "Price",
+		type: "number",
+		placeholder: "100",
+	},
+	{
+		id: "input14",
+		label: "Category",
+		type: "text",
+		placeholder: "Computers",
+	},
+	{
+		id: "input15",
+		label: "Stock",
+		type: "text",
+		placeholder: "In Stock",
+	},
+];
