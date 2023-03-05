3905e182cdb85183a000ad8679d15612fa870ca2
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-05 00:07:35 +0000

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ import SinglePage from "./Pages/Single/SinglePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { productInputs, userInputs } from "./formSource";
 import NotFound from "./Components/NotFound";
-import "./index.scss";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { themeContextProvider } from "./context/darkModeContext";
 import useResize from "./Hooks/useResize";
+import "./index.scss";
 
 function App() {
 	const { dark } = useContext(themeContextProvider);
 	const { size } = useResize();
 
-	return (
-		<div className={dark ? "" : "dark"}>
+	return size <= 1400 ? (
+		<h1 className="mx-auto flex h-screen max-w-md items-center justify-center px-2 text-center text-2xl font-bold md:text-3xl">
+			You must be on at least 1400px screen and above to view this page
+		</h1>
+	) : (
+		<div className={`${!dark ? "" : "dark"} mx-auto max-w-[1440px]`}>
 			<Router>
 				<Routes>
 					<Route path="/">
