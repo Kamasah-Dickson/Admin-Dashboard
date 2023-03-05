738f97a4827402dc1c00bbb92e523ee2604b2d0d
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-03-05 00:06:25 +0000

    update newPage

--- src/Pages/New/NewPage.tsx
+++ src/Pages/New/NewPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "../../Components/Navbar";
 import Sidebar from "../../Components/Sidebar";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
@@ -15,29 +16,57 @@ interface inputProp {
 	title: string;
 }
 
+type Files = {
+	onChange: (files: FileList | null) => void;
+};
 function NewPage({ title, inputs }: inputProp) {
+	const [file, setFile] = useState<File | null>(null);
+	console.log(file, typeof file);
+
+	function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+		const newFile = event.target.files?.[0];
+		if (newFile) {
+			setFile(newFile);
+		}
+	}
+
 	return (
 		<div className="flex w-full">
 			<Sidebar />
-			<div className="flex-[6]">
+			<div className="flex-[5]">
 				<Navbar />
 				<div className="p-5">
-					<div className="shadow-sm">
+					<div className="mb-2">
 						<h1 className="text-2xl text-[lightgrey]">{title}</h1>
 					</div>
-					<div className="mt-5 flex w-full items-center justify-between p-5 shadow-md">
+					<div className="mx-auto mt-5 flex w-full items-center justify-between gap-3 p-5 shadow-md">
 						<div className="flex-1">
-							<div className="flex h-[100px] w-[100px] cursor-pointer items-center justify-center rounded-full bg-[grey]">
-								<CameraAltIcon fontSize="large" />
-							</div>
+							{file ? (
+								<div className="mx-auto flex h-[250px] w-[250px] cursor-pointer items-center justify-center rounded-full bg-[grey]">
+									<img
+										className="h-full w-full rounded-full object-cover"
+										src={URL.createObjectURL(file)}
+										alt=""
+									/>
+								</div>
+							) : (
+								<div className="mx-auto  flex h-[250px] w-[250px] cursor-pointer items-center justify-center rounded-full bg-[grey]">
+									<CameraAltIcon fontSize="large" />
+								</div>
+							)}
 						</div>
 						<div className="flex-[2]">
 							<form className="flex flex-wrap justify-around gap-6">
 								<div className="w-[40%]">
 									<label htmlFor="file" className="cursor-pointer">
-										Image: <DriveFolderUploadIcon />
+										Image: <DriveFolderUploadIcon fontSize="large" />
 									</label>
-									<input className="hidden" type="file" id="file" />
+									<input
+										onChange={handleFileChange}
+										className="hidden"
+										type="file"
+										id="file"
+									/>
 								</div>
 								{inputs.map((data) => {
 									return (
